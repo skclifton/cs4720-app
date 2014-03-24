29674008ef76b365caf5340ab0440cbba515d2c4
csg3mf <csg3mf>
2014-03-23 21:51:51 -0400

    Removed extra file

--- PhotoInteering/gen/com/photointeering/R.java
+++ PhotoInteering/gen/com/photointeering/R.java
@@ -223,30 +223,23 @@ containing a value of this type.
         public static final int photointeering=0x7f02001c;
     }
     public static final class id {
-        public static final int TableRow=0x7f050014;
-        public static final int action_forgot_password=0x7f050018;
-        public static final int action_settings=0x7f050019;
+        public static final int TableRow=0x7f05000d;
+        public static final int action_forgot_password=0x7f050011;
+        public static final int action_settings=0x7f050012;
         public static final int currentLatTextView=0x7f050007;
         public static final int currentLocationRow=0x7f050005;
         public static final int currentLocationTextView=0x7f050006;
         public static final int currentLonTextView=0x7f050008;
-        public static final int email=0x7f05000f;
         public static final int hybrid=0x7f050004;
-        public static final int image=0x7f050015;
-        public static final int latitudeTextView=0x7f050016;
-        public static final int login=0x7f050011;
-        public static final int login_form=0x7f05000e;
-        public static final int login_status=0x7f05000c;
-        public static final int login_status_message=0x7f05000d;
-        public static final int longitudeTextView=0x7f050017;
-        public static final int newGameButton=0x7f050013;
+        public static final int image=0x7f05000e;
+        public static final int latitudeTextView=0x7f05000f;
+        public static final int longitudeTextView=0x7f050010;
+        public static final int newGameButton=0x7f05000c;
         public static final int none=0x7f050000;
         public static final int normal=0x7f050001;
-        public static final int password=0x7f050010;
         public static final int photoScrollView=0x7f05000a;
         public static final int photoScrollViewTable=0x7f05000b;
         public static final int satellite=0x7f050002;
-        public static final int sign_in_button=0x7f050012;
         public static final int tableRow1=0x7f050009;
         public static final int terrain=0x7f050003;
     }
@@ -255,9 +248,8 @@ containing a value of this type.
     }
     public static final class layout {
         public static final int activity_game_map=0x7f030000;
-        public static final int activity_login=0x7f030001;
-        public static final int activity_main=0x7f030002;
-        public static final int game_map_row=0x7f030003;
+        public static final int activity_main=0x7f030001;
+        public static final int game_map_row=0x7f030002;
     }
     public static final class menu {
         public static final int login=0x7f0a0000;
